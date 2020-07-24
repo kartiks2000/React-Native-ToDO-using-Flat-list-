cec06b9cdebd2fba4567737020c706239a952c06
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-24 14:09:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput ,FlatList,ScrollView} from 'react-native';
+
+import Input from './components/input/input';
+import List from './components/output/list';
+
+export default function App() {
+
+  const [list,setlist] = useState([]);
+
+  const adding = (item) => {
+    var temp = [...list,{item : item , key : item}];
+    setlist(temp);
+  }
+
+  const deleting = (id) => {
+    var temp = [...list];
+    temp.map((i,idx) => {
+        if(i.key == id){
+          temp.splice(idx,1);
+        }
+    });
+    setlist(temp);
+  }
+
+  return (
+    <View>
+      <View>
+        <Input add={(item) => adding(item)}></Input>
+      </View> 
+        <View>
+          <FlatList data={list} renderItem={(i) =>  (<List it={i.item.item} key={i.item.key} del={(id) => deleting(i.item.key)}/>) }/>
+        </View> 
+    </View>
+  );
+}
+
